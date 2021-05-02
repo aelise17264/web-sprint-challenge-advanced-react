e9f24e26c6ed02a7f4dfbf628a0fdde508b913cd
unknown <aelise17264@gmail.com>
2021-05-02 12:35:01 -0600

    Updates

---
+++ precache-manifest.6ad7746d517fd582d4b6614d5b71f511.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4387df96d6936981537803d83b7664e",
+    "url": "/web-sprint-challenge-advanced-react/tree/anna-brander/index.html"
+  },
+  {
+    "revision": "4a10a14bdf2b5af153e0",
+    "url": "/web-sprint-challenge-advanced-react/tree/anna-brander/static/css/main.507c6ee7.chunk.css"
+  },
+  {
+    "revision": "8c198bcfed5b5d3077a2",
+    "url": "/web-sprint-challenge-advanced-react/tree/anna-brander/static/js/2.e88083ae.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/web-sprint-challenge-advanced-react/tree/anna-brander/static/js/2.e88083ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a10a14bdf2b5af153e0",
+    "url": "/web-sprint-challenge-advanced-react/tree/anna-brander/static/js/main.75165e4f.chunk.js"
+  },
+  {
+    "revision": "c8f0319106a7e9d39f47",
+    "url": "/web-sprint-challenge-advanced-react/tree/anna-brander/static/js/runtime-main.ab3df205.js"
+  }
+]);
